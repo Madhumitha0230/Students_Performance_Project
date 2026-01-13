74ba08d5e42eb613c3ff1de482121a5a76767117
Madhumitha <madhumitha@gmail.com>
2026-01-13 21:13:27 +0530

    Student Performance Analysis Project

---
+++ student_analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("student_performances.csv")
+print(df.head())
+df["Average_Score"] = (
+    df["Math_Score"] +
+    df["Reading_Score"] +
+    df["Writing_Score"]
+) / 3
+df[["Math_Score", "Reading_Score", "Writing_Score"]].mean().plot(kind="bar",color=["skyblue","lightgreen","salmon"])
+avg = df[["Math_Score", "Reading_Score", "Writing_Score"]].mean()
+avg.plot(kind="bar",color=["skyblue","lightgreen","salmon"])
+df.groupby("Gender")[["Math_Score", "Reading_Score", "Writing_Score"]].mean().plot(kind="bar")
+plt.title("Average Scores by Subject")
+plt.ylabel("Score")
+plt.xlabel("Subject")
+
+for i,v in enumerate(avg):
+    plt.text(i,v+1,str(round(v,1)),ha='center')
+plt.show()
